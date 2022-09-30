2d91a32be669a750c7f5ebd53ef543b203b54c1c
Harish Kumar <harishstudio877@gmail.com>
2022-09-30 15:23:30 +0530

    home page completed with responsive

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
-$(document).ready(function () {
-    // $('#box').text('hello world');
-});
+// $(document).ready(function () {
+// });
+
+const bar = document.getElementById('bar')
+const close = document.getElementById('close')
+const navbar = document.getElementById('navbar')
+if (bar) {
+    bar.addEventListener('click', () => {
+        navbar.classList.add('active');
+    })
+}
+if (close) {
+    close.addEventListener('click', () => {
+        navbar.classList.remove('active');
+    })
+}
